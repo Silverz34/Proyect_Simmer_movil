670496a1e5c0b994c5cfad7c26c6038a4c61398f
Hornte3113 <alix.montesinos@gmail.com>
2025-11-11 15:20:35 -0600

    Haciendo la pantalla de carga con una animación

--- app/src/main/java/com/alixmontesinos/app_simmer/ui/screens/SplashScreen.kt
+++ app/src/main/java/com/alixmontesinos/app_simmer/ui/screens/SplashScreen.kt
@@ -1,17 +1,24 @@
 package com.alixmontesinos.app_simmer.ui.screens
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.animateLottieCompositionAsState
+import com.airbnb.lottie.compose.rememberLottieComposition
 import com.alixmontesinos.app_simmer.R
 
 @Composable
@@ -23,17 +30,24 @@ fun SplashScreen() {
             modifier = Modifier.fillMaxSize(),
             contentScale = ContentScale.Crop
         )
-        Column(
-            modifier = Modifier.fillMaxSize(),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Image(
-                painter = painterResource(id = R.drawable.logo),
-                contentDescription = "Logo de Simmer"
-            )
-            Text(text = "simmer")
-        }
+
+        Text(
+            text = "simmer",
+            modifier = Modifier
+                .align(Alignment.TopCenter)
+                .padding(top = 150.dp) // Puedes ajustar esta distancia
+        )
+
+        val composition by rememberLottieComposition(LottieCompositionSpec.RawRes(R.raw.logo_animation))
+        val progress by animateLottieCompositionAsState(composition)
+        LottieAnimation(
+            composition = composition,
+            progress = { progress },
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .fillMaxWidth()
+                .height(600.dp) // Ajusta la altura de la animación
+        )
     }
 }
 
