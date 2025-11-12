f465f13e97b26d2a96a099258f5f52fe3f80df30
Luis Ali <243682@ids.upchiapas.edu.mx>
2025-11-12 00:55:49 -0600

    Primera vista terminada

--- app/src/main/java/com/alixmontesinos/app_simmer/MainActivity.kt
+++ app/src/main/java/com/alixmontesinos/app_simmer/MainActivity.kt
@@ -14,7 +14,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             App_simmerTheme {
-                SplashScreen()
+                Login()
             }
         }
     }
---
+++ app/src/main/java/com/alixmontesinos/app_simmer/ui/theme/Fuentes.kt
@@ -0,0 +1,16 @@
+package com.alixmontesinos.app_simmer.ui.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import com.alixmontesinos.app_simmer.R
+
+
+val NunitoSans = FontFamily(
+    Font(R.font.nunito_sans_variable)
+)
+
+
+val ItimFont = FontFamily(
+    Font(R.font.itim_regular, FontWeight.Normal)
+)
