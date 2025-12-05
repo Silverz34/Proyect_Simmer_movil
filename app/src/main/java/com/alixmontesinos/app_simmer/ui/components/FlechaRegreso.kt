c1218b251137589859d519bca397047b4eb71387
Silverz34 <pradogordillomichael@gmail.com>
2025-12-05 01:04:54 -0600

    cambiiando el evento del componente FlechaRegreso

--- app/src/main/java/com/alixmontesinos/app_simmer/ui/components/FlechaRegreso.kt
+++ app/src/main/java/com/alixmontesinos/app_simmer/ui/components/FlechaRegreso.kt
@@ -1,31 +1,24 @@
 package com.alixmontesinos.app_simmer.ui.components
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.Icon
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import androidx.navigation.NavController
-
 
 @Composable
 fun FlechaRegreso(
-    navController: NavController
+    onBackClick: () -> Unit
 ) {
     Box(
         modifier = Modifier
@@ -33,11 +26,11 @@ fun FlechaRegreso(
             .size(50.dp)
             .shadow(6.dp, CircleShape)
             .background(Color.White, CircleShape)
-            .clickable {  navController.popBackStack()},
+            .clickable {  onBackClick()},
         contentAlignment = Alignment.Center
     ) {
         Icon(
-            imageVector = Icons.Filled.ArrowBack,
+            imageVector = Icons.Default.ArrowBack,
             contentDescription = null,
             tint = Color.Black,
             modifier = Modifier.size(28.dp)
