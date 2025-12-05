74ed3771aecbf6460f7e14282ab8192141e0c799
Silverz34 <pradogordillomichael@gmail.com>
2025-12-05 00:33:18 -0600

    cambiando vistas para visibilidad del bottombar en el navigation

--- app/src/main/java/com/alixmontesinos/app_simmer/ui/navigation/MainScreen.kt
+++ app/src/main/java/com/alixmontesinos/app_simmer/ui/navigation/MainScreen.kt
@@ -2,16 +2,29 @@ package com.alixmontesinos.app_simmer.ui.navigation
 
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.alixmontesinos.app_simmer.ui.components.BottomNavigation.BottomNavigationBar
+import com.alixmontesinos.app_simmer.ui.components.BottomNavigation.items
+import com.alixmontesinos.app_simmer.ui.components.BottomNavigation.items_menu
 
 @Composable
 fun MainScreen() {
     val navController = rememberNavController()
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentRoute = navBackStackEntry?.destination?.route
 
+    val pantallasConBarra = listOf(
+        items_menu.Home.ruta,
+        items_menu.Crear.ruta,
+        items_menu.Favorit.ruta,
+        items_menu.Perfil.ruta
+    )
     Scaffold(
         bottomBar = {
-            BottomNavigationBar(navController = navController)
+            if (currentRoute in pantallasConBarra){
+            BottomNavigationBar(navController = navController)}
         }
     ) { paddingValues ->
         NavHostContainer(
