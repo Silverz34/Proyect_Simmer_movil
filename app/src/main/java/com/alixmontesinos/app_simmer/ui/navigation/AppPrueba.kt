ada68b67afea5fa0f0d06bef7e25a1a891249fbc
Silverz34 <pradogordillomichael@gmail.com>
2025-12-09 00:31:55 -0600

    modificacion de rutas

--- app/src/main/java/com/alixmontesinos/app_simmer/ui/navigation/AppPrueba.kt
+++ app/src/main/java/com/alixmontesinos/app_simmer/ui/navigation/AppPrueba.kt
@@ -12,12 +12,18 @@ import com.alixmontesinos.app_simmer.ui.screens.UserApp.Home
 import com.alixmontesinos.app_simmer.ui.screens.UserAuth.Welcome
 import androidx.navigation.navArgument
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.compose.runtime.getValue
+import com.alixmontesinos.app_simmer.ui.components.BottomNavigation.BottomNavigationBar
 import com.alixmontesinos.app_simmer.ui.components.BottomNavigation.items_menu
+import androidx.compose.runtime.collectAsState
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.alixmontesinos.app_simmer.ui.screens.PerfilUser.EditarDescrip
+import com.alixmontesinos.app_simmer.ui.screens.PerfilUser.EditarPerfil
+import com.alixmontesinos.app_simmer.ui.screens.PerfilUser.Perfil
 import com.alixmontesinos.app_simmer.ui.screens.RecipeDetailScreen
-
+import com.alixmontesinos.app_simmer.ui.screens.ViewModelScreen.DescriptionViewModel
 
 
 @Composable
@@ -25,6 +31,8 @@ fun AppPrueba(){
     val navController = rememberNavController()
     val navBackStackEntry by navController.currentBackStackEntryAsState()
     val currentRoute = navBackStackEntry?.destination?.route
+    val descriptionViewModel: DescriptionViewModel = viewModel()
+    val descripcion by descriptionViewModel.descripcion.collectAsState()
 
     val bottomBarRoutes = listOf(
         items_menu.Home.ruta,
@@ -33,10 +41,10 @@ fun AppPrueba(){
         items_menu.Perfil.ruta
     )
 
-    androidx.compose.material3.Scaffold(
+    Scaffold(
         bottomBar = {
             if (currentRoute in bottomBarRoutes) {
-                com.alixmontesinos.app_simmer.ui.components.BottomNavigation.BottomNavigationBar(navController)
+                BottomNavigationBar(navController)
             }
         }
     ) { paddingValues ->
@@ -59,39 +67,41 @@ fun AppPrueba(){
                 Register(navController)
             }
 
-
-
-            composable (route= OtrasRutas.Home.route){
-                Home(navController)
-            }
-            
             // Bottom Bar Routes
             composable(items_menu.Home.ruta) { Home(navController) }
-            composable(items_menu.Crear.ruta) { CrearReceta(onBackClick = { navController.popBackStack() }) } // Using actual screen
+            composable(items_menu.Crear.ruta) { CrearReceta(onBackClick = { navController.popBackStack() }) }
             composable(items_menu.Favorit.ruta) { com.alixmontesinos.app_simmer.ui.screens.UserApp.Favorit(navController) }
             composable(items_menu.Perfil.ruta) {
-                com.alixmontesinos.app_simmer.ui.screens.PerfilUser.Perfil(
-                    onEditClick = { navController.navigate(OtrasRutas.EditarPerfil.route) }
+                Perfil(
+                    onEditClick = {navController.navigate(OtrasRutas.EditarPerfil.route)},
+                    descripcion
                 )
             }
 
-            composable (route = OtrasRutas.EditarPerfil.route) {
-                com.alixmontesinos.app_simmer.ui.screens.PerfilUser.EditarPerfil(
+
+            composable(route = OtrasRutas.EditarPerfil.route) {
+                EditarPerfil(
+                    currentDescription = descripcion,
+                    onDescriptionClick = {navController.navigate(OtrasRutas.EditarDescrip.route)},
                     onBackClick = { navController.popBackStack() }
                 )
             }
 
-           /* composable(route = OtrasRutas.EditarDescrip.route ){
+            composable(route = OtrasRutas.EditarDescrip.route) {
                 EditarDescrip(
-
+                    currentDescription = descripcion,
+                    onSaveClick = { nuevoTexto ->
+                        descriptionViewModel.updateDescripcion(nuevoTexto)
+                        navController.popBackStack()
+                    },
+                    onCancelClick = { navController.popBackStack() }
                 )
-            }*/
+            }
 
             composable(
-                route = "detail/{recipeId}", // Definimos que acepta un parámetro
+                route = "detail/{recipeId}",
                 arguments = listOf(navArgument("recipeId") { type = androidx.navigation.NavType.StringType })
             ) { backStackEntry ->
-                // Obtenemos el ID que nos pasaron
                 val recipeId = backStackEntry.arguments?.getString("recipeId") ?: ""
                 RecipeDetailScreen(navController, recipeId)
             }
