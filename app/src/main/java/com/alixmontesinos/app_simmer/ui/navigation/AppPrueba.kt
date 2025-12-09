d08c167c037ad7372f28b097c4805593052f7cf2
Hornte3113 <alix.montesinos@gmail.com>
2025-12-09 13:43:30 -0600

    tat

--- app/src/main/java/com/alixmontesinos/app_simmer/ui/navigation/AppPrueba.kt
+++ app/src/main/java/com/alixmontesinos/app_simmer/ui/navigation/AppPrueba.kt
@@ -14,6 +14,7 @@ import androidx.navigation.navArgument
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.collectAsState
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.compose.runtime.getValue
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -50,7 +51,7 @@ fun AppPrueba(){
     ) { paddingValues ->
         NavHost(
             navController = navController,
-            startDestination = OtrasRutas.Welcome.route,
+            startDestination = OtrasRutas.SplashScreen2.route,
             modifier = androidx.compose.ui.Modifier.padding(paddingValues)
         ) {
 
